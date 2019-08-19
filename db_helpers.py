d6ae0f5d72d26b637aa9c6e6e5da9288e34870ec
house <housesellout@gmail.com>
2019-08-19 16:10:48 -0400

    :- misc logging and param cleanup from previous commit

--- db_helpers.py
+++ db_helpers.py
@@ -83,7 +83,7 @@ def close_database_connection():
 
 def procValidatePIN(strPIN=''):
     funcname = f'({__filename}) procValidatePIN({strPIN})'
-    logenter(funcname, simpleprint=False, tprint=True)
+    logenter(funcname, simpleprint=False, tprint=False)
 
     #============ open db connection ===============#
     global cur
@@ -96,26 +96,18 @@ def procValidatePIN(strPIN=''):
 
     #============ perform db query ===============#
     try:
-        strOutResult = ''
-#        argsTup = (strPIN,strOutResult)
         argsTup = (strPIN,'p_out')
-        procArgs = cur.callproc(f"ValidatePIN", argsTup)
-        loginfo(funcname, f" >> '{funcname}' RESULT procArgs: {procArgs};", simpleprint=True)
-        rowCnt = cur.execute(f"select @_ValidatePIN_1;")
+        strProc = 'ValidatePIN'
+        strOutParam = '@_ValidatePIN_1'
+        procArgs = cur.callproc(f'{strProc}', argsTup)
+        rowCnt = cur.execute(f"select {strOutParam};")
         rows = cur.fetchall()
         
-#        rowCnt = cur.execute(f"call ValidatePIN({strPIN});")
-#        rows = cur.fetchall()
-        loginfo(funcname, f" >> '{funcname}' RESULT rowCnt: {rowCnt};", simpleprint=True)
-
-        print(f' >> Printing... rows', *rows, sep='\n ')
-        print(f' >> Printing... rows[0]:', rows[0])
-#        result = rows
-#        lst_result = [elem for elem in rows[0].values()]
-#        result = int(rows[0]['result'])
-        result = int(rows[0]['@_ValidatePIN_1'])
-    
-#        result = procArgs
+        loginfo(funcname, f" >> RESULT 'call {strProc}' procArgs: {procArgs};", simpleprint=True)
+        loginfo(funcname, f" >> RESULT 'call {strProc}' rowCnt: {rowCnt};", simpleprint=True)
+        loginfo(funcname, f' >> Printing... rows', *rows, simpleprint=True)
+        loginfo(funcname, f' >> Printing... rows[0]:', rows[0], simpleprint=True)
+        result = int(rows[0][strOutParam])
     except Exception as e: # ref: https://docs.python.org/2/tutorial/errors.html
         #============ handle db exceptions ===============#
         strE_0 = f"Exception hit... \nFAILED to call '{funcname}'; \n\nreturning -1"
