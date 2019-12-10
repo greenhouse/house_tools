b994bdcd7540185907f87d5b23ff1f6fdcc24fab
house <housesellout@gmail.com>
2019-12-09 19:52:37 -0500

    :- added constants.py with basic string printout variables

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from .print_helpers import *
 from .email_smtp import *
 from .term_print import *
 from .db_helpers import *
+from .constants import *
 #from sites import *
 
 #============================================================================#
